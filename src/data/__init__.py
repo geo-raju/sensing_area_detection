156c90a8ce178e250ade1921d9b3985afc49bb82
Geo Raju <geo.raju@icloud.com>
2025-07-18 12:49:38 +0100

    refactor(data): Streamline data processing package exports
    - Removed `SensingAreaDataset` and transform functions from `src/data/__init__.py` exports.
    - Moved `dataset.py` and `transforms.py` out of `src/data/` directory.
    - This change simplifies the public interface of the `data` package, focusing solely on `DataOrganiser`.

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,6 +1,4 @@
 """Data processing package."""
 from .organiser import DataOrganiser
-from .dataset import SensingAreaDataset
-from .transforms import get_train_transform, get_val_transform
 
-__all__ = ['DataOrganiser', 'SensingAreaDataset', 'get_train_transform', 'get_val_transform']
+__all__ = ['DataOrganiser']
